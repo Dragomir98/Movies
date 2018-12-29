acd81b996f83d3aa639acd1a75ed6136583a3359
Dragomir98 <dragomir.stoychev98@gmail.com>
2018-12-29 23:58:05 +0200

    Added layout and default messages

--- resources/views/include/messages.blade.php
+++ resources/views/include/messages.blade.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: HP
- * Date: 28.12.2018 г.
- * Time: 13:39 ч.
- */
+@if(count($errors) > 0)
+    @foreach($errors->all() as $error)
+        <div class="alert alert-danger">
+            {{$error}}
+        </div>
+    @endforeach
+@endif
+
+@if(session('success'))
+    <div class="alert alert-success">
+        {{session('success')}}
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger">
+        {{session('error')}}
+    </div>
+@endif
