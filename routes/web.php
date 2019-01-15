d285056382f258c348ba37e3a7dd163d999cee42
Dragomir98 <dragomir.stoychev98@gmail.com>
2019-01-16 00:41:14 +0200

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Input;
+use App\Movies;
 
 //Route::get('/', 'PagesController@index');
 //Route::get('/about', 'PagesController@about');
@@ -28,5 +20,15 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/prod', function ()
     {
-        return view('producerList', ['movies'=>'']);
+        $movies = DB::table('movies')->get();
+        return view('producerList', ['movies'=>$movies]);
     });
+
+Route::get('/gen', function ()
+    {
+        $movies = DB::table('movies')->get();
+        return view('genreList', ['movies'=>$movies]);
+    });
+
+
+Route::get('/search', 'MoviesController@search')->name('movies');
